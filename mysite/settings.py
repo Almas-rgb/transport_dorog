b071a17209fe8eaa273a744bfb60ac6d836d5b28
Almas-rgb <70622090+Almas-rgb@users.noreply.github.com>
2021-06-23 13:01:14 +0600

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@2=wzslgbgv%=a1xnj+lw!2oxh6)b3$$wh!f^dqj*d+!z+-p!m'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'transportidorog.herokuapp.com', '127.0.0.1' 
+    'transportidorog.herokuapp.com', '127.0.0.1', 'localhost' 
 ]
 
 
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'seven6496@gmail.com'
-EMAIL_HOST_PASSWORD = 'vorona07'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
